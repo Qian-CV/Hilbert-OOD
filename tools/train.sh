2507fa4b585b664cddc1bd9b1e3f006ac930284c
Jeremy Wang_ubuntu <jeremy.wang0126@gmail.com>
2025-04-18 01:05:15 +0800

    修改：     tools/train.sh

--- tools/train.sh
+++ tools/train.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 export PYTHONPATH="/media/ubuntu/nvidia/wlq/co_work1_hilbert/HERO:$PYTHONPATH"
 
+#python train.py \
+#/media/ubuntu/nvidia/wlq/co_work1_hilbert/HERO/configs/_HERO/HERO-le90_r50_fpn_6x_hrsc2016.py
 python train.py \
-/media/ubuntu/nvidia/wlq/co_work1_hilbert/HERO/configs/_HERO/oriented-rcnn-le90_r50_fpn_6x_hrsc2016.py
-python train.py \
-/media/ubuntu/nvidia/wlq/co_work1_hilbert/HERO/configs/_HERO/HERO-le90_r50_fpn_6x_hrsc2016.py
+/media/ubuntu/nvidia/wlq/co_work1_hilbert/HERO/configs/_HERO/HERO-le90_r50_fpn_6x_hrsc2016.py \
+--cfg-options model.rpn_head.use_cross_attention=True \
+--work-dir ./work_dirs/hrsc/HERO/HERO-le90_r50_fpn_6x_hrsc2016_lr5e3_hilbert_conv1*3_cross-attn/
 #python train.py \
 #/media/ubuntu/nvidia/wlq/part2/mmrotate/configs/_wlq/t2det/rtmdet_baseline/VEDAI/t2det_rtmdet_m-6x-vedai.py \
 #--cfg-options model.bbox_head.ss_loss_start=0.2 model.bbox_head.loss_scale_ss.loss_weight=0 \
